637c2bd43f62576082918caf035e129567c21c3f
Agnik Saha <agniksaha2019@gmail.com>
2023-09-08 08:55:57 +0600

    first commit

---
+++ build/precache-manifest.048c80dd0d1e2687a97af1d1cf818bf9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1360c792b8b612a163579c48fde449",
+    "url": "/agniksaha/index.html"
+  },
+  {
+    "revision": "13db95e868b6e0d09822",
+    "url": "/agniksaha/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "98b58e3b6253184dea89",
+    "url": "/agniksaha/static/css/main.3cb9f9db.chunk.css"
+  },
+  {
+    "revision": "13db95e868b6e0d09822",
+    "url": "/agniksaha/static/js/2.d0b4698a.chunk.js"
+  },
+  {
+    "revision": "c0fa58a94cdbab3827ec663ce548f3aa",
+    "url": "/agniksaha/static/js/2.d0b4698a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b58e3b6253184dea89",
+    "url": "/agniksaha/static/js/main.acf90595.chunk.js"
+  },
+  {
+    "revision": "a0e40ffc2d7ba7ae792e",
+    "url": "/agniksaha/static/js/runtime-main.5755e8aa.js"
+  },
+  {
+    "revision": "8fb0e5fd721a02afda190255b5ee649d",
+    "url": "/agniksaha/static/media/about.8fb0e5fd.png"
+  },
+  {
+    "revision": "fa03b2f8f7b99367702f0e0c8c5460ba",
+    "url": "/agniksaha/static/media/clock.fa03b2f8.svg"
+  },
+  {
+    "revision": "0751b7b090824e8c615c52fc4ea45c1c",
+    "url": "/agniksaha/static/media/diaphragm.0751b7b0.svg"
+  },
+  {
+    "revision": "5c3b33d18614383cdb67197a1a0e8585",
+    "url": "/agniksaha/static/media/man2.5c3b33d1.png"
+  },
+  {
+    "revision": "d6ab8f5256b68b7d5d89f22e79f5aaf8",
+    "url": "/agniksaha/static/media/money.d6ab8f52.svg"
+  },
+  {
+    "revision": "4d4ad81686b7e5d24ef457eb7187a689",
+    "url": "/agniksaha/static/media/teamwork.4d4ad816.svg"
+  }
+]);
